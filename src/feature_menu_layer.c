a8490135ba65d182f9834a491ec5ebc993ebd9aa
stroy1 <stroy1@binghamton.edu>
2015-03-21 13:47:46 -0400

    Contact JS

--- src/feature_menu_layer.c
+++ src/feature_menu_layer.c
@@ -6,11 +6,56 @@
 #define NUM_SECOND_MENU_ITEMS 1
 
 static Window *s_main_window;
-static MenuLayer *s_menu_layer;
+static MenuLayer *s_menu_layer[NUM_FIRST_MENU_ITEMS];
 static GBitmap *s_menu_icons[NUM_MENU_ICONS];
 static GBitmap *s_background_bitmap;
 
+static char * words[5] = {"h", "e", "l", "l", "o"};
+
 static int s_current_icon = 0;
+
+
+
+// BEGIN CODE FOR SENDING MESSAGES
+static void send_cmd(void) {
+  Tuplet value = TupletInteger(1, 1);
+  DictionaryIterator *iter;
+  app_message_outbox_begin(&iter);
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "Text: SEND_CMD");
+
+  if (iter == NULL) return;
+  dict_write_tuplet(iter, &value);
+  dict_write_end(iter);
+  app_message_outbox_send();
+}
+
+void out_sent_handler(DictionaryIterator *sent, void *context) {
+  // outgoing message was delivered
+  DictionaryIterator *iter;
+  app_message_outbox_begin(&iter);
+}
+
+void out_failed_handler(DictionaryIterator *failed, AppMessageResult reason, void *context) {
+  // outgoing message failed
+  DictionaryIterator *iter;
+  app_message_outbox_begin(&iter);
+}
+
+static void in_received_handler(DictionaryIterator *iter, void *context) {
+  Tuple *location_tuple = dict_read_first(iter); // Check for fields you expect to receive
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "RECEIVE: %s", location_tuple->value->cstring);
+  
+  if (location_tuple) { // Act on the found fields received
+    words[0] = location_tuple->value->cstring;
+  }
+}
+
+
+void in_dropped_handler(AppMessageResult reason, void *context) {
+  // incoming message dropped
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "Message Dropped");
+}
+// END CODE FOR SENDING MESSAGES
 
 static uint16_t menu_get_num_sections_callback(MenuLayer *menu_layer, void *data) {
   return NUM_MENU_SECTIONS;
@@ -52,7 +97,7 @@ static void menu_draw_row_callback(GContext* ctx, const Layer *cell_layer, MenuI
       switch (cell_index->row) {
         case 0:
           // This is a basic menu item with a title and subtitle
-          menu_cell_basic_draw(ctx, cell_layer, "Basic Item", "With a subtitle", NULL);
+          menu_cell_basic_draw(ctx, cell_layer, words[0], "subs", NULL);
           break;
         case 1:
           // This is a basic menu icon with a cycling icon
@@ -88,6 +133,11 @@ static void menu_select_callback(MenuLayer *menu_layer, MenuIndex *cell_index, v
       // After changing the icon, mark the layer to have it updated
       layer_mark_dirty(menu_layer_get_layer(menu_layer));
       break;
+    case 0:
+      //words[0] = "H";
+      send_cmd();
+      APP_LOG(APP_LOG_LEVEL_DEBUG, "Before layer");
+      layer_mark_dirty(menu_layer_get_layer(menu_layer));
   }
 
 }
@@ -106,8 +156,8 @@ static void main_window_load(Window *window) {
   GRect bounds = layer_get_frame(window_layer);
 
   // Create the menu layer
-  s_menu_layer = menu_layer_create(bounds);
-  menu_layer_set_callbacks(s_menu_layer, NULL, (MenuLayerCallbacks){
+  s_menu_layer[0] = menu_layer_create(bounds);
+  menu_layer_set_callbacks(s_menu_layer[0], NULL, (MenuLayerCallbacks){
     .get_num_sections = menu_get_num_sections_callback,
     .get_num_rows = menu_get_num_rows_callback,
     .get_header_height = menu_get_header_height_callback,
@@ -117,14 +167,14 @@ static void main_window_load(Window *window) {
   });
 
   // Bind the menu layer's click config provider to the window for interactivity
-  menu_layer_set_click_config_onto_window(s_menu_layer, window);
+  menu_layer_set_click_config_onto_window(s_menu_layer[0], window);
 
-  layer_add_child(window_layer, menu_layer_get_layer(s_menu_layer));
+  layer_add_child(window_layer, menu_layer_get_layer(s_menu_layer[0]));
 }
 
 static void main_window_unload(Window *window) {
   // Destroy the menu layer
-  menu_layer_destroy(s_menu_layer);
+  menu_layer_destroy(s_menu_layer[0]);
 
   // Cleanup the menu icons
   for (int i = 0; i < NUM_MENU_ICONS; i++) {
@@ -148,6 +198,17 @@ static void deinit() {
 }
 
 int main(void) {
+  // START MESSAGE CODE
+  app_message_register_inbox_received(in_received_handler);
+  app_message_register_inbox_dropped(in_dropped_handler);
+  //app_message_register_outbox_sent(out_sent_handler);
+  //app_message_register_outbox_failed(out_failed_handler);
+
+  //const uint32_t inbound_size = 256;
+  //const uint32_t outbound_size = 256;
+  app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum());
+  // END MESSAGE CODE
+  
   init();
   app_event_loop();
   deinit();
